212cd953f1166db129926e8fdd2919a792d3509c
nalo26 <nath.v26@gmail.com>
2023-06-20 11:12:48 +0200

    Operator url path error

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ PORT = 8091
 
 
 def send_json(data: dict, algorithm: str, params: dict):
+    algorithm = algorithm.replace(":", "/")
     url = f"http://{IP}:{PORT}/{algorithm}"
     print("Sending data to ", url)
     req = rq.post(url, params=params, json=data)
